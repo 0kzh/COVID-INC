5923e5614d55deba06031cf20ee2a96129853b42
Kelvin Zhang <me@kelvinzhang.ca>
2020-03-16 05:18:07 +0000

    Add HTTPS

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 // dependencies
 var express = require('express');
 var http = require('http');
+const https = require('https');
 const request = require('request');
 var fs = require('fs');
-
+var ioServer = require('socket.io');
 var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/covidinc.io/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/covidinc.io/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/covidinc.io/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+// start http and https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+var io = new ioServer();
+io.attach(httpServer);
+io.attach(httpsServer);
+
 
 // serve front-end website
+app.all('*', httpsRedir);
 app.use(express.static('./client'));
+
+function httpsRedir(req, res, next) {
+  if(req.secure){
+    return next();
+  };
+  res.redirect('https://' + req.hostname + req.url);
+}
 
 function formatDate(date) {
   var d = new Date(date),
@@ -43,7 +69,13 @@ io.on('connection', function(socket){
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, function () {
-  console.log('server running on port ' + PORT);
+
+
+const PORT = process.env.NODE_ENV == 'production' ? 80 : 3000;
+httpServer.listen(PORT, function () {
+  console.log('HTTP server running on port ' + PORT);
+});
+
+httpsServer.listen(443, function () {
+  console.log('HTTPS server running on port 443');
 });
