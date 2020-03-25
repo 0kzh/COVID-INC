0c9737fc7acea4314d0c4b0fc638326a0efe1d99
WilliamLQin <william.qin51@gmail.com>
2020-03-25 16:36:54 -0400

    fix news scroll updating

--- client/static/js/news.js
+++ client/static/js/news.js
@@ -42,14 +42,26 @@ function cycleNews() {
     if (currentIndex >= todayNews.length) {
         currentIndex = 0;
     }
-    setTimeout(function () {
+}
+
+const resetNews = () => {
+    console.log("Resetting news scroll...");
+    $('#news-headline').text("");
+    $('#news-headline').removeClass("text");
+    setTimeout(() => {  
+        $('#news-headline').addClass("text"); 
         cycleNews();
-    }, 10000);
+        clearInterval(window.newsTimer);
+        window.newsTimer = setInterval(() => {
+            cycleNews();
+        }, 10000);
+    }, 10);
 }
 
 function isValidDate(date) {
     return date && date instanceof Date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
 }
+
 // window.newsData is json with key: id, data: date, headline, link
 // in: date is a formatted date (call formatDate()) or a Date() object
 function fillNewsBar(date) {
@@ -70,8 +82,7 @@ function fillNewsBar(date) {
     newsExists = true;
     todayNews = window.newsData[formattedDate];
     currentIndex = 0;
-    $('#news-headline').text(todayNews[currentIndex]['headline']);
-    $('#news-headline').attr("class", "text");
+    resetNews();
 
     console.log("News headlines found:");
     console.log(todayNews);
@@ -104,15 +115,11 @@ function populateNews() {
 
     $("[id^='newshead']").click(function() {
         var key = $(this).attr('id');
-        // console.log(key);
         key = key.substring(9);
-        // console.log(key);
     
         $("[id='newsdesc-" + key + "']").toggle();
     });
 
-    // fillNewsBar(window.day);
-    cycleNews();
     newsLoaded = true;
 
     console.log("News populated.");
@@ -140,28 +147,3 @@ function setNewsToCurrDate() {
     }
 
 }
-
-// window.newsData = {
-//     "2020-03-20": [
-//         {
-//             headline: "UK closes pubs, cafes and bars, and announces other measures",
-//             description: `<p>– In his daily briefing, the UK Prime Minister Boris Johnson announced that cafes, pubs, and bars to close, as well as shops, theatres and leisure centres, are to close to protect public health.</p>
-//             <p>– The government are deferring taxes to the end of the financial year, as well as waiving VAT for some businesses. It will also provide a job retention scheme to pay 80% of wages for those who cannot work due to the pandemic.</p>
-//             <p>&nbsp;</p>
-//             <p>Syria banned entry of visitors from multiple countries affected by the coronavirus pandemic. The government said that the cases in the nation remain zero.</p>`
-//         },
-//         {
-//             headline: "Germany cases jump to 13,957",
-//             description: `<p>– Germany’s Robert Koch Institute (RKI) reported that the number of cases surged by 2,958 to 13,957.</p>
-//             <p>– The number of deaths increased by 11 to 31.</p>`
-//         },
-//         {
-//             headline: "Spain’s deaths cross 1,000",
-//             description: `<p>– The death toll in Spain increased to 1,002 from 767, said the country’s health emergencies chief Fernando Simon .</p>
-//             <p>– The number of cases jumped to 19,980 from 17,147 on Thursday.<strong><br>
-//             </strong></p>`
-//         }
-//     ]
-// };
-
-// populateNews();
